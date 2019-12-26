32575ffd17dd70e4244b7d0e5414843baf8b7eed
Locketgoma <gom@kiwing.co.kr>
2019-12-27 04:47:34 +0900

    기본 항목 업데이트

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraMove : MonoBehaviour
+{
+    public Camera cameraObject;
+    public GameObject BackgroundMap;
+    
+    [Range(-5,5)]
+    public float dragSpeed = 2;
+
+    [Range(0,48)]
+    public float lockMovement = 18; // 카메라 움직임 범위 제한
+
+    [SerializeField]
+    private Vector3 dragOrigin;
+    private bool locking=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (Input.GetMouseButtonDown(0)&&locking==false)
+        {
+            dragOrigin = Input.mousePosition;
+            locking = true;
+            return;
+        }
+        if (Input.GetMouseButtonUp(0)||locking==false)
+        {       //???
+            locking = false;
+            return;
+        }
+
+
+        Vector3 pos = cameraObject.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, 0, 0);            //'움직이고 싶은 정도'를 담게 됨. 좌 -> 우 : +, 우 -> 좌 : -
+
+
+        Debug.Log(move.x);
+
+        if (Math.Abs(transform.position.x+move.x) > lockMovement)       //가끔 튕겨져나가는? 버그같은게 보이는데 나중에 확인해놓을것.
+        {
+            move = new Vector3(0, 0, 0);
+        }
+
+
+        transform.Translate(move, Space.World);                         //월드 좌표계 기준 move vector 값 만큼 이동.
+        BackgroundMap.transform.Translate(move, Space.World);
+
+    }
+
+}
---
+++ Assets/Scripts/MobControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("기본 수치")]
+    [Range(0,100)]
+    public int Damage;          //공격력
+    [Range(0,10000)]
+    public int Health;          //체력
+    [Range(0,100)]
+    public int Defence;         //방어력
+    [Range(1, 10)]
+    public int AttackSpeed;     //공격 빈도 (5초당 N회)
+
+    //데미지 공식 : (공격력 / 방어력)+1, 크리티컬시 2.5배 데미지, 크리티컬 확률 공식 = ?
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
