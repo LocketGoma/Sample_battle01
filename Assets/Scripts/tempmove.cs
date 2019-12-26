e950eec48d66a16dc03b75634428386fcf391737
Locketgoma <gom@kiwing.co.kr>
2019-12-26 19:55:06 +0900

    코딱지만큼 갱신

---
+++ Assets/Scripts/tempmove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempmove : MonoBehaviour   
+{
+    public GameObject Mob;
+    public float speed=1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + (speed/100), transform.position.y, transform.position.z);
+    }
+}
