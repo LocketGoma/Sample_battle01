68f0fdcce2eca0ecde9edf01e8aaf7e3310ff245
Locketgoma <gom@kiwing.co.kr>
2020-01-14 02:24:35 +0900

    주석만 추가

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//https://www.reddit.com/r/Unity3D/comments/8he04r/peer_2_peer_multiplayer/
+//https://docs.unity3d.com/kr/current/Manual/UNetGettingStarted.html
+//https://www.youtube.com/watch?v=cQ0f1_Ct9lc
+//https://stackoverflow.com/questions/33457100/built-in-multiplayer-in-unity3d-without-a-server
+//https://docs.unity3d.com/Manual/UNetManager.html
+public class NetworkManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
